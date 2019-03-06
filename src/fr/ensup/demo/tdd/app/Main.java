a76dc462377ac8a79adc7d04d33827d022360c45
Rudy <rudy.tourrou@live.fr>
2019-03-06 17:02:12 +0100

    1.0.1 - Added failing tests

---
+++ src/fr/ensup/demo/tdd/app/Main.java
@@ -0,0 +1,8 @@
+package fr.ensup.demo.tdd.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/fr/ensup/demo/tdd/test/TestCalculator.java
@@ -0,0 +1,28 @@
+package fr.ensup.demo.tdd.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class TestCalculator {
+
+    @Test
+    public void addIntegers() {
+        fail("Erreur d'addition.");
+    }
+
+    @Test
+    public void subtractIntegers() {
+        fail("Erreur de soustraction.");
+    }
+
+    @Test
+    public void divideIntegers() {
+        fail("Erreur de division.");
+    }
+
+    @Test
+    public void multiplyIntegers() {
+        fail("Erreur de multiplication.");
+    }
+}
